8a1c25530291253a61dc0aceae2657a47aa7eef9
Jores Amancio <joresamancio@Joress-MacBook-Air.local>
2024-10-14 12:55:12 +0800

    Added Vercel

--- blog-nest/src/Users/user.service.ts
+++ blog-nest/src/Users/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { FirebaseRepository } from 'src/FirebaseService/firebase.service';
+import { FirebaseRepository } from '../FirebaseService/firebase.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
--- blog-nest/src/main.ts
+++ blog-nest/src/main.ts
@@ -1,19 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS with dynamic origin checking
+  const allowedOrigins = [
+    'http://localhost:5173',
+    'https://blogs-vue-d76df.web.app',
+  ];
+
   app.enableCors({
     origin: (origin, callback) => {
-      const allowedOrigins = [
-        'http://localhost:5173',
-        'https://blogs-vue-d76df.web.app',
-      ];
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error(`Not allowed by CORS: ${origin}`));
@@ -21,6 +21,12 @@ async function bootstrap() {
     },
     credentials: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+  });
+  app.enableCors({
+    origin: 'https://blogs-vue-d76df.web.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
 
   app.use(cookieParser());
@@ -33,7 +39,9 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  const port = process.env.PORT || 8080;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
